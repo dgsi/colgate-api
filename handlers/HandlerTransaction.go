75665ea0ac751b21bd6469ac91cf965bf21d5d5b
user <user@users-MacBook-Pro.local>
2016-04-23 17:09:08 +0800

    fointed query to view table

--- handlers/HandlerTransaction.go
+++ handlers/HandlerTransaction.go
@@ -89,7 +89,8 @@ func (handler TransactionHandler) ShowStationTransactions(c *gin.Context) {
 
 		if station.StationId != "" {	
 			query := []m.TransactionMember{}	
-			handler.db.Raw("SELECT m.member_id,m.member_fname,m.member_lname,m.member_country_region,m.member_city,m.member_email_address,m.member_mobile,t.station_id,t.date_created,t.date_updated FROM wp_members AS m INNER JOIN transaction as t ON t.member_id=m.member_id where t.station_id=?",station_id).Scan(&query)
+			handler.db.Raw("SELECT * from qry_membertransactions WHERE staion_id = ?",station_id).Scan(&query)
+			//handler.db.Raw("SELECT m.member_id,m.member_fname,m.member_lname,m.member_country_region,m.member_city,m.member_email_address,m.member_mobile,t.station_id,t.date_created,t.date_updated FROM wp_members AS m INNER JOIN transaction as t ON t.member_id=m.member_id where t.station_id=?",station_id).Scan(&query)
 			c.JSON(http.StatusOK, &query)
 		} else {
 			respond(http.StatusBadRequest,"Station not found!",c,true)
